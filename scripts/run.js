f688a2166fe7e393b660fb913837cec48b7ee731
Kyronne <kyronne.parkes@gmail.com>
2022-11-12 20:13:30 +0000

    changed names to make deploying contract easier

--- scripts/run.js
+++ scripts/run.js
@@ -1,33 +1,33 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-  
-    console.log("Contract address of deployment:", waveContract.address);
-    console.log("Contract deployed and owned by:", owner.address);
-  
-    await waveContract.getTotalWaves();
-  
-    const firstWave = await waveContract.wave();
-    await firstWave.wait();
-  
-    await waveContract.getTotalWaves();
-  
-    const secondWave = await waveContract.connect(randomPerson).wave();
-    await secondWave.wait();
-  
-    await waveContract.getTotalWaves();
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  const firstWaveTxn = await waveContract.wave();
+  await firstWaveTxn.wait();
+
+  await waveContract.getTotalWaves();
+
+  const secondWaveTxn = await waveContract.connect(randomPerson).wave();
+  await secondWaveTxn.wait();
+
+  await waveContract.getTotalWaves();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
